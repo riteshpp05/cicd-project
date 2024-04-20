393ac7fd0d18b7717775355a9a4c798f10392913
riteshpp05 <147828381+riteshpp05@users.noreply.github.com>
2024-04-20 14:26:44 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,11 @@
 import pytest
 from app import app
 
-
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
-
 
 def test_app_is_working(client):
     response = client.get('/')
